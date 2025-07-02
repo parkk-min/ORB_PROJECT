f1f556584d372039411a9cef4e85760ade9f2bc3
Jooeon-kim <james5459@naver.com>
2025-07-02 17:54:23 +0900

    변경사항 result

--- lib/result.dart
+++ lib/result.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class Result extends StatelessWidget {
+class Result extends StatefulWidget {
   const Result({super.key});
+
+  @override
+  State<Result> createState() => _ResultState();
+}
+
+class _ResultState extends State<Result> {
+  late String result;
+
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    result = ModalRoute.of(context)?.settings.arguments as String;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +26,17 @@ class Result extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              width: 200,
-              height: 200,
+              width: 300,
+              height: 300,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text("당신의 결과는?"),
-                  Text("두번째 줄")
+                  Text("멋진 게임이었습니다.", style: TextStyle(fontSize: 20),),
+                  Text("게임의 결과는?",style: TextStyle(fontSize: 20),),
+                  Icon(result=="승리"?Icons.celebration:Icons.face_unlock_rounded,
+                    size: 30,
+                  ),
+                  Text(result,style: TextStyle(fontSize: 20),),
                 ],
               ),
             ),
@@ -27,7 +46,7 @@ class Result extends StatelessWidget {
                 Container(
                   child: ElevatedButton(
                       onPressed:(){
-                        
+                        Navigator.pushNamed(context, "/");
                       },
                       child: Text("홈으로"))
                 ),
@@ -35,7 +54,7 @@ class Result extends StatelessWidget {
                 Container(
                     child: ElevatedButton(
                         onPressed:(){
-                          
+                          Navigator.pop(context,"reset");
                         },
                         child: Text("다시하기"))
                 ),
